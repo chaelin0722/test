f4c5cfe5ec1fcd27dd9f9bd2587d401496f75f27
chaelin0722 <ccaa9697@gmail.com>
2020-05-31 09:16:31 +0000

    연결완료

---
+++ chaelin.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+
+	//cout<<"hello chaelin"<<endl;
+
+	printf("chaelin is working\n");
+	return 0;
+}
---
+++ chaelin.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+	cout<<"chaelin"<<" hello"<<endl;
+
+	return 0;
+}
